eb940bcaedcd8c486e65c9616e10b6e40f86b1ba
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-05-31 22:52:01 -0300

    feat: ensure ShortenerController call UrlRepository with correct values

--- src/presentation/controller/shortener.ts
+++ src/presentation/controller/shortener.ts
@@ -2,9 +2,12 @@ import { type Controller } from '../protocols/controller'
 import { type HttpRequest, type HttpResponse } from '../protocols/http'
 import { type Shortener } from '../../data/usecases/protocols/shortener'
 import { badRequest, ok } from '../helpers/http-helpers'
+import { type AddUrlRepository } from '../../data/usecases/protocols/add-url-repository'
 
 export class ShortenerController implements Controller {
-  constructor (private readonly shortener: Shortener) {}
+  constructor (private readonly shortener: Shortener,
+    private readonly addUrlRepository: AddUrlRepository) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.url) {
       return badRequest(new Error())
@@ -12,6 +15,11 @@ export class ShortenerController implements Controller {
     const { url } = httpRequest.body
     const short = await this.shortener.shorten(url)
     const host = process.env.HOST || 'http://localhost:8080'
+
+    await this.addUrlRepository.add({
+      original: url,
+      shorten: short
+    })
     return ok({
       original: url,
       short: `${host}/${short}`
