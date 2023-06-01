b1c3f1a84effe3da006051ae2ec0a2d01c30397b
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-05-31 21:25:07 -0300

    feat: ensure ShortenerController calls Shortener with correct value

--- src/presentation/controller/shortener.ts
+++ src/presentation/controller/shortener.ts
@@ -1,7 +1,9 @@
 import { type Controller } from '../protocols/controller'
 import { type HttpRequest, type HttpResponse } from '../protocols/http'
+import { type Shortener } from '../../data/usecases/protocols/shortener'
 
 export class ShortenerController implements Controller {
+  constructor (private readonly shortener: Shortener) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.url) {
       const httpResponse = {
@@ -10,12 +12,13 @@ export class ShortenerController implements Controller {
       }
       return httpResponse
     }
+    const { url } = httpRequest.body
+    await this.shortener.shorten(url)
     return await new Promise((resolve) => {
       resolve({
         statusCode: 200,
         body: {}
       })
-    }
-    )
+    })
   }
 }
