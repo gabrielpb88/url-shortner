a2dcc54cf390e584f54375acef32bb45fb2550e1
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-05-31 21:25:20 -0300

    test: ensure ShortenerController calls Shortener with correct value

--- src/presentation/controller/shortener.spec.ts
+++ src/presentation/controller/shortener.spec.ts
@@ -1,16 +1,35 @@
 import { ShortenerController } from './shortener'
 import { type HttpRequest } from '../protocols/http'
+import { type Shortener } from '../../data/usecases/protocols/shortener'
 
 interface SutTypes {
   sut: ShortenerController
+  shortenerStub: Shortener
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new ShortenerController()
+  const shortenerStub = makeShortenerStub()
+  const sut = new ShortenerController(shortenerStub)
   return {
-    sut
+    sut,
+    shortenerStub
   }
 }
+
+const makeShortenerStub = (): Shortener => {
+  class ShortenerStub implements Shortener {
+    async shorten (value: string): Promise<string> {
+      return await Promise.resolve('')
+    }
+  }
+  return new ShortenerStub()
+}
+
+const makeHttpRequest = (): HttpRequest => ({
+  body: {
+    url: 'http://any_url.com'
+  }
+})
 
 describe('Shortener UseCase', () => {
   test('Should return 400 when no url is provided', async () => {
@@ -20,5 +39,13 @@ describe('Shortener UseCase', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual({ statusCode: 400, body: new Error() })
+  })
+
+  test('Should call Shortener with correct value', async () => {
+    const { sut, shortenerStub } = makeSut()
+    const shortenerSpy = jest.spyOn(shortenerStub, 'shorten')
+    const httpRequest = makeHttpRequest()
+    await sut.handle(httpRequest)
+    expect(shortenerSpy).toHaveBeenCalledWith(httpRequest.body.url)
   })
 })
