bd85e2f0f52bc74136c00d51292ac670e5fb0137
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2025-06-10 14:40:10 -0300

    feat: add logger using pino and update MongoDB connection error handling

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -1,25 +1,25 @@
 import { type Collection, type Db, MongoClient } from 'mongodb'
 import { type Url } from '../interfaces/url.interface'
-
-const url = process.env.MONGO_URL
-const dbName = process.env.MONGO_DB_NAME
-const client = new MongoClient(url)
+import { logger } from 'logger'
 
 export async function connectToMongo(): Promise<{
   db: Db
   urlsCollection: Collection<Url>
 }> {
   try {
+    const url = process.env.MONGO_URL
+    const dbName = process.env.MONGO_DB_NAME
+    const client = new MongoClient(url)
+
     await client.connect()
-    console.log('Connected to MongoDB')
 
     const db = client.db(dbName)
     return {
       db,
-      urlsCollection: db.collection<Url>('urls')
+      urlsCollection: db.collection<Url>('urls'),
     }
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error)
+    logger.error(error, 'Error connecting to MongoDB')
     throw error
   }
 }
