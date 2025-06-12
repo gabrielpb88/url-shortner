e7987698c25c8f884162f96b553c29e45d31fc76
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2025-06-12 19:46:51 -0300

    test: enhance UrlService tests by adding deleteExpiredUrls and restructuring existing tests

--- src/services/url.service.spec.ts
+++ src/services/url.service.spec.ts
@@ -6,47 +6,84 @@ describe('getOriginalUrl', () => {
   let collectionMock: jest.Mocked<Collection<Url>>
   let service: UrlService
 
-  it('should return the original URL when shortUrl is found', async () => {
+  beforeAll(() => {
     collectionMock = {
       findOne: jest.fn(),
+      insertOne: jest.fn(),
+      deleteMany: jest.fn(),
     } as unknown as jest.Mocked<Collection<Url>>
-
     service = new UrlService(collectionMock)
-
-    const mockShortUrl = 'any-url'
-    const mockOriginalUrl = 'https://example.com'
-    collectionMock.findOne.mockResolvedValue({
-      shortUrl: mockShortUrl,
-      original: mockOriginalUrl,
-    } as unknown as Url)
-
-    const result = await service.getOriginalUrl(mockShortUrl)
-
-    expect(collectionMock.findOne).toHaveBeenCalledWith({
-      shortUrl: mockShortUrl,
-    })
-    expect(result).toBe(mockOriginalUrl)
+    process.env.EXPIRATION_TIME = '1'
   })
 
-  it('should return null when shortUrl is not found', async () => {
-    const mockShortUrl = 'not-found-short-url'
-    collectionMock.findOne.mockResolvedValue(null)
+  describe('getOriginalUrl', () => {
+    it('should return the original URL when shortUrl is found', async () => {
+      const mockShortUrl = 'any-url'
+      const mockOriginalUrl = 'https://example.com'
+      collectionMock.findOne.mockResolvedValue({
+        shortUrl: mockShortUrl,
+        original: mockOriginalUrl,
+      } as unknown as Url)
 
-    const result = await service.getOriginalUrl(mockShortUrl)
+      const result = await service.getOriginalUrl(mockShortUrl)
 
-    expect(collectionMock.findOne).toHaveBeenCalledWith({
-      shortUrl: mockShortUrl,
+      expect(collectionMock.findOne).toHaveBeenCalledWith({
+        shortUrl: mockShortUrl,
+      })
+      expect(result).toBe(mockOriginalUrl)
     })
-    expect(result).toBeUndefined()
+
+    it('should return null when shortUrl is not found', async () => {
+      const mockShortUrl = 'not-found-short-url'
+      collectionMock.findOne.mockResolvedValue(null)
+
+      const result = await service.getOriginalUrl(mockShortUrl)
+
+      expect(collectionMock.findOne).toHaveBeenCalledWith({
+        shortUrl: mockShortUrl,
+      })
+      expect(result).toBeUndefined()
+    })
+
+    it('should throw an error if collection.findOne fails', async () => {
+      const mockShortUrl = 'any short url'
+      const error = new Error('Database failure')
+      collectionMock.findOne.mockRejectedValue(error)
+
+      await expect(service.getOriginalUrl(mockShortUrl)).rejects.toThrow(
+        'Database failure'
+      )
+    })
   })
 
-  it('should throw an error if collection.findOne fails', async () => {
-    const mockShortUrl = 'any short url'
-    const error = new Error('Database failure')
-    collectionMock.findOne.mockRejectedValue(error)
+  describe('shortenUrl', () => {
+    it('should insert a new URL and return the generated shortUrl', async () => {
+      jest.spyOn(service as any, 'generateShortUrl').mockReturnValue('hash')
 
-    await expect(service.getOriginalUrl(mockShortUrl)).rejects.toThrow(
-      'Database failure'
-    )
+      const originalUrl = 'http://www.any-url.com'
+      const result = await service.shortenUrl(originalUrl)
+
+      expect(collectionMock.insertOne).toHaveBeenCalledWith({
+        original: originalUrl,
+        shortUrl: 'hash',
+        createdAt: expect.any(Date),
+        expiresAt: expect.any(Date),
+      })
+      expect(result).toBe('hash')
+    })
+  })
+
+  describe('deleteExpiredUrls', () => {
+    it('should delete all expired URLs', async () => {
+      const now = new Date()
+      collectionMock.deleteMany.mockResolvedValue({
+        acknowledged: true,
+        deletedCount: 1,
+      })
+      await service.deleteExpiredUrls()
+      expect(collectionMock.deleteMany).toHaveBeenCalledWith({
+        expiresAt: { $lt: now },
+      })
+    })
   })
 })
