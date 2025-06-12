79fd511c67592d18de57ee91345f294700cb57ca
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2025-06-12 17:28:30 -0300

    refactor: refactor routes to use controller methods

--- src/routes/url.routes.ts
+++ src/routes/url.routes.ts
@@ -1,29 +1,13 @@
 import { Router } from 'express'
 import { type UrlService } from '../services/url.service'
+import { UrlController } from 'controllers/url.controller'
 
 export function createUrlRoutes(urlService: UrlService): Router {
   const router = Router()
+  const urlController = new UrlController(urlService)
 
-  router.post('/', async (req, res) => {
-    const { url } = req.body
-    if (!url) {
-      return res.status(400).json({ error: 'URL is required' })
-    }
-    const shortUrl = await urlService.shortenUrl(url)
-    res.status(201).json({ shortUrl })
-  })
-
-  router.get('/:shortUrl', async (req, res) => {
-    const { shortUrl } = req.params
-
-    const originalUrl = await urlService.getOriginalUrl(shortUrl)
-
-    if (originalUrl) {
-      res.redirect(originalUrl)
-    } else {
-      res.status(404).json({ error: 'Short URL not found' })
-    }
-  })
+  router.post('/', urlController.shortenUrl)
+  router.get('/:shortUrl', urlController.getOriginalUrl)
 
   return router
 }
